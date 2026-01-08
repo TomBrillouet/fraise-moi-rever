df806eb3082c0815543d27ef8343700321e49de8
Tom <tbrillouet@tennispro.fr>
2026-01-08 17:38:57 +0100

    ♻️F01 : LOGIN >create LoginForm

---
+++ src/components/pages/login/LoginForm.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+export default function LoginForm() {
+  //state
+  const [inputValue, setInputValue] = useState("");
+  //comportements
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue("");
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  return (
+    <form action="submit" onSubmit={handleSubmit}>
+      <h1>Bienvenue chez nous ! </h1>
+      <br />
+      <h2>Connectez vous</h2>
+      <input
+        type="text"
+        placeholder="Entrez votre prénom"
+        value={inputValue}
+        onChange={handleChange}
+        required
+      />
+      <button>Accéder à votre espace</button>
+    </form>
+  );
+}
