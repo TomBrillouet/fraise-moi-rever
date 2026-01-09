025fc92888b4a7a5dfbf4ee69451ac785376c86f
Tom <tbrillouet@tennispro.fr>
2026-01-09 10:33:55 +0100

    ✨Fo2 : ORDER > add username

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,8 +1,11 @@
-import { Link } from "react-router";
+import { Link, useParams } from "react-router";
 export default function OrderPage() {
+  //state
+  const { inputValue } = useParams();
+
   return (
     <div>
-      <h1>Bonjour </h1>
+      <h1>Bonjour {inputValue}</h1>
       {/* Prénom rentrer dans la page login */}
       <Link to="/">
         <button>Déconnexion</button>
