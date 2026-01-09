0f1291c53b0bee904de17c33b98831e264bc8383
Tom <tbrillouet@tennispro.fr>
2026-01-09 09:11:39 +0100

    ✨F02: ORDER > create OrderPage

---
+++ src/components/pages/order/OrderPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function OrderPage() {
+  return (
+    <div>
+      <h1>Bonjour prénom</h1>
+      {/* Prénom rentrer dans la page login */}
+      <button>Déconnexion</button>
+      {/* Redirection vers la page Login */}
+    </div>
+  );
+}
