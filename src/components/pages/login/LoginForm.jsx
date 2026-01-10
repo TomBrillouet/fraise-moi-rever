7a7b64542579a4903920a9309eb85d9ef992644b
Tom <tbrillouet@tennispro.fr>
2026-01-10 21:15:10 +0100

    ♻️F03: LoginForm > Refacto

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 import { useNavigate } from "react-router"
 import styled from "styled-components"
 import { theme } from "../../../theme"
+import { BsPersonCircle } from "react-icons/bs"
+import { GoChevronRight } from "react-icons/go"
 
 export default function LoginForm() {
   //state
@@ -19,31 +21,65 @@ export default function LoginForm() {
   }
   return (
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenue chez nous ! </h1>
+      <h1 className="amatic-sc-bold">Bienvenue chez nous ! </h1>
       <br />
       <hr />
-      <h2>Connectez vous</h2>
-      <input
-        type="text"
-        placeholder="Entrez votre prénom"
-        value={inputValue}
-        onChange={handleChange}
-        required
-      />
-      <button>Accéder à votre espace</button>
+      <h2 className="amatic-sc-bold">Connectez vous</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="Entrez votre prénom"
+          value={inputValue}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button>
+        Accéder à mon espace
+        <GoChevronRight />
+      </button>
     </LoginFormStyled>
   )
 }
 
 const LoginFormStyled = styled.form`
+  color: ${theme.colors.white};
   display: flex;
   flex-direction: column;
-  color: ${theme.colors.white};
+  align-items: center;
+  padding: 40px 32px;
+  h1 {
+    font-size: ${theme.fonts.P5};
+    margin: 0;
+  }
   hr {
-    color: ${theme.colors.primary_burger};
+    width: 100%;
+    height: 3px;
+    border: none;
+    background: #f56a2c;
+  }
+  h2 {
+    font-size: ${theme.fonts.P4};
+  }
+  input {
+    margin-bottom: 18px;
+    padding: 18px 24px;
+    border-radius: ${theme.borderRadius.round};
+    border: none;
   }
   button {
     background-color: ${theme.colors.primary_burger};
     color: ${theme.colors.white};
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: 700;
+    font-size: ${theme.fonts.P0};
+    padding: 18px 106.2px;
+    border-radius: ${theme.borderRadius.round};
+    border: none;
+    &:hover {
+      background-color: ${theme.colors.white};
+      color: ${theme.colors.primary_burger};
+      cursor: pointer;
+    }
   }
 `
