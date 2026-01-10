0347e6e8c5da8bc39df20a639e334cba81454146
Tom <tbrillouet@tennispro.fr>
2026-01-10 21:04:24 +0100

    💄F03: LoginPage > styles css

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,6 +1,33 @@
-import LoginForm from "./LoginForm";
+import styled from "styled-components"
+import Logo from "../../Logo"
+import LoginForm from "./LoginForm"
 
 export default function LoginPage() {
   //render
-  return <LoginForm />;
+  return (
+    <LoginPageStyled>
+      <div className="background"></div>
+      <Logo />
+      <LoginForm />
+    </LoginPageStyled>
+  )
 }
+
+const LoginPageStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  justify-content: center;
+  div.background {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: -1;
+    background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+      url("/src/assets/images/F03 burger-background.jpg");
+
+    background-size: cover;
+    background-position: center;
+  }
+`
