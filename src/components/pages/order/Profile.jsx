f7c581b82b60453c7ff0c44a4b86c4f2a65ce16c
Tom <tbrillouet@tennispro.fr>
2026-01-14 15:00:57 +0100

    ♻️F04 : Navbar > create profile component

---
+++ src/components/pages/order/Profile.jsx
@@ -0,0 +1,44 @@
+import { Link, useParams } from "react-router"
+import { BsPersonCircle } from "react-icons/bs"
+import { theme } from "../../../theme"
+import styled from "styled-components"
+
+export default function Profile() {
+  const { username } = useParams()
+
+  return (
+    <ProfileStyled>
+      <div>
+        <p>
+          Hey, <span>{username}</span>
+        </p>
+        <Link to="/">Se déconnecter</Link>
+      </div>
+      <BsPersonCircle />
+    </ProfileStyled>
+  )
+}
+const ProfileStyled = styled.div`
+  display: flex;
+  gap: 10px;
+  font-family: "Open Sans", sans-serif;
+  font-weight: ${theme.fonts.weights.regular};
+  font-size: 16px;
+  color: ${theme.colors.greyBlue};
+  p {
+    margin: 0;
+  }
+  span {
+    font-weight: ${theme.fonts.weights.bold};
+    color: ${theme.colors.primary};
+  }
+  a {
+    cursor: pointer;
+    font-size: ${theme.fonts.size.XXS};
+    color: ${theme.colors.greyBlue};
+  }
+  svg {
+    height: ${theme.fonts.size.P4};
+    width: ${theme.fonts.size.P4};
+  }
+`
