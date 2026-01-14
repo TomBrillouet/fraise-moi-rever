4dad2eb40c70f46d43161e8d5eafaa78d1d54f0e
Tom <tbrillouet@tennispro.fr>
2026-01-14 14:04:16 +0100

    ♻️F04: ORDER > Navbar > Create NavbarRightSide

---
+++ src/components/pages/order/NavbarRightSide.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components"
+import { theme } from "../../../theme"
+import { Link, useParams } from "react-router"
+import { BsPersonCircle } from "react-icons/bs"
+
+export default function NavbarRightSide() {
+  const { username } = useParams()
+
+  return (
+    <NavbarRightSideStyled>
+      <div>
+        <p>
+          Hey, <span>{username}</span>
+        </p>
+        <Link to="/">Se déconnecter</Link>
+      </div>
+      <BsPersonCircle />
+    </NavbarRightSideStyled>
+  )
+}
+const NavbarRightSideStyled = styled.div`
+  font-family: "Open Sans", sans-serif;
+  font-weight: ${theme.fonts.weights.regular};
+  font-size: 16px;
+  color: ${theme.colors.greyBlue};
+  display: flex;
+  gap: 10px;
+  p {
+    margin: 0;
+  }
+  span {
+    font-weight: ${theme.fonts.weights.bold};
+    color: ${theme.colors.primary};
+  }
+  a {
+    cursor: pointer;
+    font-size: ${theme.fonts.size.XXS};
+    color: ${theme.colors.greyBlue};
+  }
+  svg {
+    height: ${theme.fonts.size.P4};
+    width: ${theme.fonts.size.P4};
+  }
+`
--- src/components/reusable/NavBar.jsx
+++ src/components/reusable/NavBar.jsx
@@ -1,27 +1,15 @@
-import { useNavigate, useParams } from "react-router"
-import { BsPersonCircle } from "react-icons/bs"
 import Logo from "./Logo"
 import styled from "styled-components"
 import { theme } from "../../theme"
+import NavbarRightSide from "../pages/order/NavbarRightSide"
 
 export default function NavBar() {
-  const { username } = useParams()
-  const navigate = useNavigate()
-  const handleClick = () => {
-    navigate("/")
-  }
   return (
     <NavBarStyled>
-      <Logo />
-      <div className="user-info">
-        <div>
-          <p>
-            Hey, <span>{username}</span>
-          </p>
-          <a onClick={handleClick}>Se déconnecter</a>
-        </div>
-        <BsPersonCircle />
+      <div className="left-side">
+        <Logo />
       </div>
+      <NavbarRightSide />
     </NavBarStyled>
   )
 }
@@ -35,28 +23,5 @@ const NavBarStyled = styled.nav`
   padding: 0 70px 0 ${theme.spacing.md};
   a {
     text-decoration: none;
-  }
-  .user-info {
-    font-family: "Open Sans", sans-serif;
-    font-weight: ${theme.fonts.weights.regular};
-    font-size: 16px;
-    color: ${theme.colors.greyBlue};
-    display: flex;
-    gap: 10px;
-    p {
-      margin: 0;
-    }
-    span {
-      font-weight: ${theme.fonts.weights.bold};
-      color: ${theme.colors.primary};
-    }
-    a {
-      cursor: pointer;
-      font-size: ${theme.fonts.size.XXS};
-    }
-    svg {
-      height: ${theme.fonts.size.P4};
-      width: ${theme.fonts.size.P4};
-    }
   }
 `
