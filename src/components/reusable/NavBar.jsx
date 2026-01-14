04d9702f83933eaf9f75d3622f9f84c3f77b9f1c
Tom <tbrillouet@tennispro.fr>
2026-01-14 11:11:36 +0100

    ♻️F04: NavBar > create reusable navbar component & add styling orderpage

---
+++ src/components/reusable/NavBar.jsx
@@ -0,0 +1,64 @@
+import { useNavigate, useParams } from "react-router"
+import { BsPersonCircle } from "react-icons/bs"
+import Logo from "./Logo"
+import styled from "styled-components"
+import { theme } from "../../theme"
+
+export default function NavBar() {
+  const { username } = useParams()
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate("/")
+  }
+  return (
+    <NavBarStyled>
+      <a href="/">
+        <Logo />
+      </a>
+      <div className="user-info">
+        <div>
+          <p>
+            Hey, <span>{username}</span>
+          </p>
+          <a onClick={handleClick}>Se déconnecter</a>
+        </div>
+        <BsPersonCircle />
+      </div>
+    </NavBarStyled>
+  )
+}
+const NavBarStyled = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: ${theme.colors.white};
+  border-radius: ${theme.borderRadius.extraRound}
+    ${theme.borderRadius.extraRound} 0 0;
+  padding: 0 70px 0 ${theme.spacing.md};
+  a {
+    text-decoration: none;
+  }
+  .user-info {
+    font-family: "Open Sans", sans-serif;
+    font-weight: ${theme.fonts.weights.regular};
+    font-size: 16px;
+    color: ${theme.colors.greyBlue};
+    display: flex;
+    gap: 10px;
+    p {
+      margin: 0;
+    }
+    span {
+      font-weight: ${theme.fonts.weights.bold};
+      color: ${theme.colors.primary};
+    }
+    a {
+      cursor: pointer;
+      font-size: ${theme.fonts.size.XXS};
+    }
+    svg {
+      height: ${theme.fonts.size.P4};
+      width: ${theme.fonts.size.P4};
+    }
+  }
+`
