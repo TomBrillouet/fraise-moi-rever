7fbd752776b67ffa3b525a72e25cbe11695cf5bc
Tom <tbrillouet@tennispro.fr>
2026-01-14 10:09:10 +0100

    💄F04 : > ORDER > add layout

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,15 +1,50 @@
-import { Link, useParams } from "react-router";
+import { Link, useNavigate, useParams } from "react-router"
+import styled from "styled-components"
 export default function OrderPage() {
-  //state
-  const { username } = useParams();
+  const { username } = useParams()
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate("/")
+  }
 
   return (
-    <div>
-      <h1>Bonjour {username}</h1>
-      {/* Prénom rentrer dans la page login */}
-      <Link to="/">
-        <button>Déconnexion</button>
-      </Link>
-    </div>
-  );
+    <OrderPageStyled>
+      <div>
+        <nav>
+          <div className="logo">Logo</div>
+          <div>
+            <p>Hey, {username}</p>
+            <button onClick={handleClick}>Se déconnecter</button>
+          </div>
+        </nav>
+        <div className="container"></div>
+      </div>
+    </OrderPageStyled>
+  )
 }
+
+const OrderPageStyled = styled.div`
+  background: orange;
+  height: 100%;
+  width: 100%;
+  padding: 34px 0;
+  > div {
+    width: 1400px;
+    background: red;
+    justify-self: center;
+    border-radius: 15px;
+  }
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+    background: blue;
+    border-radius: 15px 15px 0 0;
+  }
+
+  div.container {
+    background: green;
+    height: 80vh;
+    border-radius: 0 0 15px 15px;
+  }
+`
