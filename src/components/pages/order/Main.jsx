0cf9e9c9ba1f8ccebf537ec9c906394a15543068
Tom <tbrillouet@tennispro.fr>
2026-01-15 14:31:52 +0100

    💄F05: ORDER > Main > Layout

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -2,13 +2,49 @@ import styled from "styled-components"
 import { theme } from "../../../theme"
 
 export default function Main() {
-  return <MainStyled></MainStyled>
+  return (
+    <MainStyled>
+      <div className="grid-products">
+        <div className="product"></div>
+        <div className="product"></div>
+        <div className="product"></div>
+        <div className="product"></div>
+        <div className="product"></div>
+        <div className="product"></div>
+        <div className="product"></div>
+        <div className="product"></div>
+        <div className="product"></div>
+        <div className="product"></div>
+        <div className="product"></div>
+        <div className="product"></div>
+        <div className="product"></div>
+        <div className="product"></div>
+        <div className="product"></div>
+      </div>
+    </MainStyled>
+  )
 }
 
 const MainStyled = styled.div`
   background: #f5f5f7;
   flex: 1;
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  overflow-y: auto;
   border-radius: 0 0 ${theme.borderRadius.extraRound}
     ${theme.borderRadius.extraRound};
+
+  .grid-products {
+    background: red;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: auto;
+    padding: 50px 0;
+    row-gap: 60px;
+    justify-items: center;
+    .product {
+      background: blue;
+      height: 330px;
+      width: 240px;
+    }
+  }
 `
