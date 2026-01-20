678aa180e39c7bef0cf85d004185a5a5ea675c17
Tom <tbrillouet@tennispro.fr>
2026-01-20 14:10:57 +0100

    💄F07: ORDER > AdminPanel> Header > create layout

--- src/components/pages/order/AdminPanel.jsx
+++ src/components/pages/order/AdminPanel.jsx
@@ -3,7 +3,11 @@ import styled from "styled-components"
 export default function AdminPanel() {
   return (
     <AdminPanelStyled>
-      <div className="header">Onglets</div>
+      <div className="header">
+        <button>..</button>
+        <button>Ajouter un produit</button>
+        <button>Modifier un produit</button>
+      </div>
       <div className="body">Ajouter un produit</div>
     </AdminPanelStyled>
   )
@@ -13,11 +17,16 @@ const AdminPanelStyled = styled.div`
   position: sticky;
   bottom: 0;
   .header {
-    background: blue;
     height: 44px;
+    padding: 0 71px;
+    button {
+      height: 100%;
+      padding: 14px 22px;
+    }
   }
   .body {
     height: 250px;
-    background: yellow;
+    background: #ffffff;
+    border: solid 1px #e4e5e9;
   }
 `
