bdda5e4fb2f8cd4970cae2363b849e9d10acacae
Tom <tbrillouet@tennispro.fr>
2026-01-24 00:21:57 +0100

    ♻️F08: CATALOG > NoProduct > refactor by creating <NoProduct/>

--- src/components/pages/order/Main/Catalog/Catalog.jsx
+++ src/components/pages/order/Main/Catalog/Catalog.jsx
@@ -3,14 +3,11 @@ import { formatPrice } from "../../../../../utils/maths"
 import Card from "../../../../reusable/Card"
 import orderContext from "../../../../../context/OrderContext.jsx"
 import { useContext } from "react"
-import PrimaryButton from "../../../../reusable/PrimaryButton.jsx"
-import { fakeMenu } from "../../../../../datas/fakeMenu"
+import NoProduct from "./NoProduct.jsx"
 
 export default function Catalog() {
-  const { products, isAdmin, setProducts } = useContext(orderContext)
-  const handleClick = () => {
-    setProducts(fakeMenu.LARGE)
-  }
+  const { products } = useContext(orderContext)
+
   const catalog = products.map(({ imageSource, title, price, id }) => (
     <Card
       image={imageSource}
@@ -20,28 +17,13 @@ export default function Catalog() {
       id={id}
     />
   ))
-  const noproduct = isAdmin ? (
-    <div className="noproduct">
-      Le menu est vide ? <br />
-      Cliquez ci-dessous pour le réinitialiser <br />
-      <PrimaryButton
-        label={"Générer de nouveaux produits"}
-        onClick={handleClick}
-      />
-    </div>
-  ) : (
-    <div className="noproduct">
-      <b>Victime de notre succès ! :d</b> <br /> De nouvelles recettes en cours
-      de préparation. <br /> à très vite !
-    </div>
-  )
 
   return (
     <CatalogStyled>
       {catalog.length >= 1 ? (
         <div className="catalog">{catalog}</div>
       ) : (
-        noproduct
+        <NoProduct />
       )}
     </CatalogStyled>
   )
@@ -58,18 +40,5 @@ const CatalogStyled = styled.div`
     padding: 50px 0;
     row-gap: 60px;
     justify-items: center;
-  }
-  .noproduct {
-    font-family: "Amatic SC", cursive;
-    font-size: 36px;
-    text-transform: uppercase;
-    position: absolute;
-    top: 35%;
-    left: 35%;
-    line-height: 60px;
-    button {
-      padding: 19px 25px;
-      width: unset;
-    }
   }
 `
