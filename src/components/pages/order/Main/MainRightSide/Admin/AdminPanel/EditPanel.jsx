0f9e15ee18ce2c794611dca1a1dd500da8dd91a2
Tom <tbrillouet@tennispro.fr>
2026-01-30 14:05:45 +0100

    🔇F09: EditPanel > Remove clg

--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/EditPanel.jsx
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/EditPanel.jsx
@@ -6,6 +6,5 @@ import { EMPTY_PRODUCT } from "../../../../../../../enums/product"
 
 export default function EditPanel() {
   const { productSelected } = useContext(OrderContext)
-  console.log(productSelected)
   return productSelected === EMPTY_PRODUCT ? <HintMessage /> : <EditForm />
 }
