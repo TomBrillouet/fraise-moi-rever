4f2f658d8148dba1d33b625af01b806a0f577575
Tom <tbrillouet@tennispro.fr>
2026-02-01 20:20:23 +0100

    💄F10 : BASKET > add styles

--- src/components/pages/order/Main/Basket.jsx
+++ src/components/pages/order/Main/Basket.jsx
@@ -1,11 +1,18 @@
 import styled from "styled-components"
+import { theme } from "../../../../theme"
+import { FaReact } from "react-icons/fa"
 
 export default function Basket() {
   return (
     <BasketStyled>
-      <div className="header">header</div>
-      <div className="main">main</div>
-      <div className="footer">footer</div>
+      <div className="header">
+        <div className="total">Total</div>
+        <div className="sum">0,00€</div>
+      </div>
+      <div className="main">Votre commande est vide.</div>
+      <div className="footer">
+        Codé avec <FaReact /> par Tom Brillouet
+      </div>
     </BasketStyled>
   )
 }
@@ -13,16 +20,37 @@ export default function Basket() {
 const BasketStyled = styled.div`
   display: grid;
   grid-template-rows: 1fr 10fr 1fr;
+  font-family: ${theme.fonts.family.stylish};
 
   .header {
-    background-color: red;
+    background-color: ${theme.colors.background_dark};
+    color: ${theme.colors.primary};
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 16px;
+    font-size: ${theme.fonts.size.P4};
+
   }
 
   .main {
-    background-color: green;
+    background-color: ${theme.colors.background_white};
+    box-shadow: 0px 0px 20px 0px #00000033 inset;
+    font-size: ${theme.fonts.size.P4};
+    color: ${theme.colors.greyBlue};
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
   .footer {
-    background-color: blue;
-  }
+    background-color: ${theme.colors.background_dark};
+    color: ${theme.colors.white};
+    text-align: center;
+    font-size: ${theme.fonts.size.P2};
+    align-content: center;
+
+    svg {
+        color:#61DBFB
+    }
 `
