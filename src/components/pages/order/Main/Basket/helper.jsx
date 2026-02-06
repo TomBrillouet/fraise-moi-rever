446f6c02a439d4732e860ce9607b22dca8857723
Tom <tbrillouet@tennispro.fr>
2026-02-07 00:06:06 +0100

    🐛 F12: BASKET > fix cases isNan

--- src/components/pages/order/Main/Basket/helper.jsx
+++ src/components/pages/order/Main/Basket/helper.jsx
@@ -3,7 +3,7 @@ import { findObjectById } from "../../../../../utils/array"
 export const calculateSumToPay = (basket, products) => {
   return basket.reduce((total, basketProduct) => {
     const catalogProduct = findObjectById(basketProduct.id, products)
-
+    if (isNaN(catalogProduct.price)) return total
     return total + basketProduct.quantity * catalogProduct.price
   }, 0)
 }
