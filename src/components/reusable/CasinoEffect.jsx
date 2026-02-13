54863ab6cd0cddfdc080f236e7c89d29ab0f02ab
Tom <tbrillouet@tennispro.fr>
2026-02-13 09:46:47 +0100

    ✨F14: BASKET > Total > Add CasinoEffect (finished)

--- src/components/reusable/CasinoEffect.jsx
+++ src/components/reusable/CasinoEffect.jsx
@@ -1,28 +1,51 @@
-import { TransitionGroup } from "react-transition-group"
+import { createRef } from "react"
+import { CSSTransition, TransitionGroup } from "react-transition-group"
 import styled from "styled-components"
 
 export default function CasinoEffect({ count, className }) {
+  const nodeRef = createRef()
   return (
     <TransitionGroup component={CasinoEffectStyled}>
-      <CSSTransition classNames={"count-animated"} timeout={5000} key={count}>
-        <span className={className}>{count}</span>
+      <CSSTransition
+        nodeRef={nodeRef}
+        classNames={"count-animated"}
+        timeout={300}
+        key={count}
+      >
+        <span ref={nodeRef} className={className}>
+          {count}
+        </span>
       </CSSTransition>
     </TransitionGroup>
   )
 }
 
 const CasinoEffectStyled = styled.div`
+  position: relative;
+  overflow: hidden;
+  display: block;
+
+  span {
+    display: block;
+    backface-visibility: hidden;
+  }
+
   .count-animated-enter {
+    transform: translateY(100%);
   }
   .count-animated-enter-active {
-  }
-  .count-animated-enter-done {
+    transform: translateY(0%);
+    transition: transform 300ms;
   }
 
   .count-animated-exit {
+    transform: translateY(0%);
+    position: absolute;
+    right: 0;
+    bottom: 0;
   }
   .count-animated-exit-active {
-  }
-  .count-animated-done {
+    transform: translateY(-100%);
+    transition: transform 300ms;
   }
 `
