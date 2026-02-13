3721f57482c905e2b46d36a5d8685bc386f6f902
Tom <tbrillouet@tennispro.fr>
2026-02-13 11:51:16 +0100

    ✨F14: ORDERPAGE > ADMIN > add animation on switch isAdmin

--- src/components/pages/order/Main/MainRightSide/MainRightSide.jsx
+++ src/components/pages/order/Main/MainRightSide/MainRightSide.jsx
@@ -1,17 +1,27 @@
-import { useContext } from "react"
+import { useContext, useRef } from "react"
 import styled from "styled-components"
 import OrderContext from "../../../../../context/OrderContext"
 import { theme } from "../../../../../theme"
 import Admin from "./Admin/Admin"
 import Catalog from "./Catalog/Catalog"
+import { CSSTransition } from "react-transition-group"
 
 export default function MainRightSide() {
   const { isAdmin } = useContext(OrderContext)
+  const nodeRef = useRef()
 
   return (
     <MainRightSideStyled>
       <Catalog />
-      {isAdmin && <Admin />}
+      <CSSTransition
+        appear={true}
+        in={isAdmin}
+        nodeRef={nodeRef}
+        classNames="animate-admin"
+        timeout={500}
+      >
+        <Admin ref={nodeRef} />
+      </CSSTransition>
     </MainRightSideStyled>
   )
 }
@@ -21,4 +31,33 @@ const MainRightSideStyled = styled.div`
   overflow-y: hidden;
   display: grid;
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+  .animate-admin-enter,
+  .animate-admin-appear {
+    transform: translateY(100px);
+    opacity: 0;
+  }
+
+  .animate-admin-enter-active,
+  .animate-admin-appear-active {
+    transform: translateY(0);
+    opacity: 1;
+    transition: 0.5s;
+  }
+
+  .animate-admin-exit {
+    transform: translateY(0);
+    opacity: 1;
+  }
+
+  .animate-admin-exit-active {
+    transform: translateY(100px);
+    opacity: 0;
+    transition: 0.5s;
+  }
+
+  .animate-admin-exit-done {
+    transform: translateY(100px);
+    opacity: 0;
+  }
 `
